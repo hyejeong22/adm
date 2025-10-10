2db448a43e6d9f21c896c4b9d1275051a07da330
hyejeong22 <hyejung525@naver.com>
2025-10-10 22:54:53 +0900

    Add files via upload

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pymysql
 
 def get_connection():
@@ -9,3 +10,16 @@ def get_connection():
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
+=======
+import pymysql
+
+def get_connection():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='너의비번',
+        db='vaprt_db',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+>>>>>>> 20f56cd (Add files via upload)
--- db_config.py
+++ db_config.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 # db_config.py
@@ -12,3 +13,19 @@ def get_connection():
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
+=======
+# -*- coding: utf-8 -*-
+
+# db_config.py
+import pymysql
+
+def get_connection():
+    return pymysql.connect(
+        host='localhost',
+        user='root',             # ← 네 MySQL 사용
+        password='1234',       # ← 네 MySQL 비밀번호
+        db='apartment',          # ← 사용할 데이터
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+>>>>>>> 20f56cd (Add files via upload)
