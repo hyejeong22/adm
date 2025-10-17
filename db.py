8da52f5ef67afbe09cc9fae6da7a9523c86e2443
hyejeong22 <hyejung525@naver.com>
2025-10-17 15:01:19 +0900

    Initial commit for V Apartment admin page

---
+++ db.py
@@ -0,0 +1,11 @@
+import pymysql
+
+def get_connection():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='너의비번',
+        db='vaprt_db',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
